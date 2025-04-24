e142cb4845f315f5aba2b4397d03a876a348742a
Ali Kagan Bertizlioglu <alikaganbertizlioglu@gmail.com>
2025-04-24 15:18:18 +0300

    commit

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
+  standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
+  closeWarning() {
+    const popup = document.querySelector('.warning-popup');
+    if (popup) {
+      popup.remove();
+    }
+  }
 }
